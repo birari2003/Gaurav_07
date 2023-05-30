bac3162c118584c2d20fc6fbd2250b00066513c7
birari2003 <124417583+birari2003@users.noreply.github.com>
2023-05-30 20:12:45 +0530

    Add files via upload

---
+++ Day4_ascii.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char n;
+    cout<<"Enter any key :";
+    cin>>n;
+
+    cout<<"The ASCII value of your key is "<<int(n)<<endl;
+    cout<<"Size of the key is :"<<sizeof(int(n));
+}
