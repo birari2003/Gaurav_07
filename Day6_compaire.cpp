15055ec173433c247b8bba3f7118017bfa92d835
birari2003 <124417583+birari2003@users.noreply.github.com>
2023-06-01 22:15:59 +0530

    Add files via upload

---
+++ Day6_compaire.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter two no.";
+    cin>>a>>b;
+    
+    cout<<"First is greater than second "<<a<<endl;
+    cout<<"Second is greater first "<<b<<endl;
+    cout<<"Both are equal "<<a<<"="<<b;
+}
